0f720b7263aa9b7eeaeb3920be4bb76a62c89f3f
José Salazar <jmsalazar.macg@hotmail.com>
2025-01-03 23:07:42 -0600

    header-burger_menu-init

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,59 @@
-import './header.module.css'
+import { useState } from "react";
+import styles from "./header.module.css";
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-export default Header
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+    console.log(hamburgerOpen);
+  };
+
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerContainer_navbar}>
+        <h1 className={styles.headerContainer_name}>José Salazar</h1>
+        <button
+          className={`${styles.headerContainer_burgerMenu} ${
+            hamburgerOpen ? styles.open : ""
+          }`}
+          onClick={toggleHamburger}
+          aria-expanded={hamburgerOpen} // Para indicar el estado del menú (importante para accesibilidad)
+          aria-label="Toggle menu" // Descripción para lectores de pantalla
+        >
+          <div className={styles.burgerMenu_bar1}></div>
+          <div className={styles.burgerMenu_bar2}></div>
+          <div className={styles.burgerMenu_bar3}></div>
+          <div className={styles.burgerMenu_bar4}></div>
+        </button>
+      </div>
+
+      <div
+        className={`${styles.headerContainer_menu} ${
+          hamburgerOpen ? styles.open : ""
+        }`}
+      >
+        <div className={styles.headerContainer_menu__content}>
+          <ul className={styles.headerContainer_menu__list}>
+            <li className={styles.headerContainer_menu__listItem}>Home</li>
+            <li className={styles.headerContainer_menu__listItem}>Projects</li>
+            <li className={styles.headerContainer_menu__listItem}>About</li>
+            <li className={styles.headerContainer_menu__listItem}>Résumé</li>
+          </ul>
+        </div>
+        <div className={styles.headerContainer_menu__sideBar}>
+          <div className={styles.headerContainer_menu__email}>
+            <a href="https://developer.mozilla.org/es/docs/Web/CSS/position">
+              Send me an email
+            </a>
+          </div>
+          <div className={styles.headerContainer_menu__socials}>
+            Redes sociales:
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
