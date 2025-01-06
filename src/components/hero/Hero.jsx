5d6156c8172cfa4f7d681bca76d76c51e4afdfcf
José Salazar <jmsalazar.macg@hotmail.com>
2025-01-05 23:04:59 -0600

    herp-section-animations

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,43 +1,70 @@
 import { useState, useEffect, useRef } from "react";
 import styles from "./hero.module.css";
-import Orb from "@/components/orb/Orb.jsx"; // Suponiendo que Orb es un componente ya implementado
+import Orb from "@/components/orb/Orb.jsx";
 
 const HeroSection = () => {
-  const [currentText, setCurrentText] = useState('');
-  const texts = ["Innovador", "Curioso", "Detallista", "Resiliente", "Jose"];
+  const [currentText, setCurrentText] = useState("CodeCrafter");
+  const [isFinalText, setIsFinalText] = useState(false);
+  const indexRef = useRef(0);
 
   useEffect(() => {
-    let index = 0;
-    console.log(index)
+    const texts = ["Dreamer", "Innovative", "Jose"];
     const interval = setInterval(() => {
-      if (index < texts.length) {
-        console.log(index)
-        console.log(texts.length)
-        setCurrentText(texts[index]);
-        index++;
+      if (indexRef.current < texts.length - 1) {
+        setCurrentText(texts[indexRef.current]);
+        indexRef.current++;
       } else {
-        clearInterval(interval); // Detenemos el intervalo cuando llegamos a "Jose"
+        setCurrentText(texts[indexRef.current]);
+        setTimeout(() => {
+          setIsFinalText(true);
+        }, 2000);
+        clearInterval(interval);
       }
     }, 2000);
-
     return () => clearInterval(interval);
-  }, [texts]);
+  }, []);
 
   return (
     <div className={styles.heroSectionContainer}>
-      <div className={styles.heroSectionContainer__text}>
-        <h1>
-          Hello! Call me{" "}
-          <span className={styles.dynamicText}>{currentText}</span>
-        </h1>
+      <div className={styles.heroSection__top}>
+        <div
+          className={
+            isFinalText
+              ? styles.heroSectionContainer__finalText
+              : styles.heroSectionContainer__text
+          }
+        >
+          <h1>
+            Hello! Call me{" "}
+            <span
+              key={currentText}
+              className={isFinalText ? styles.finalText : styles.dynamicText}
+            >
+              {currentText}
+            </span>
+            {isFinalText && (
+              <p>
+                I&#39;m a self-taught programmer who&#39;s dedicated to learning
+                and growing in technology like NextJS and React. I&#39;ve
+                tackled various personal projects that showcase my ability to
+                problem-solve and create innovative solutions. I&#39;m excited
+                to take on my first professional role and bring my skills to a
+                dynamic team and vanguard team.
+              </p>
+            )}
+          </h1>
+        </div>
+        <div className={styles.heroSectionContainer__particleOrb}>
+          <Orb
+            totalParticles={700}
+            size={400}
+            rotationSpeed={0.01}
+            particleSize={1.5}
+          />
+        </div>
       </div>
-      <div className={styles.heroSectionContainer__particleOrb}>
-        <Orb
-          totalParticles={700}
-          size={400}
-          rotationSpeed={0.01}
-          particleSize={1.5}
-        />
+      <div className={styles.heroSection__arrowDown}>
+        <div className={styles.hola}>▽hola</div>
       </div>
     </div>
   );
