4aa58a2c3d362ec937455b07e5670bc2a61e3f5c
José Salazar <jmsalazar.macg@hotmail.com>
2025-01-08 20:54:14 -0600

    logos-img

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,25 @@
-import styles from './projects.module.css'
+import styles from "./projects.module.css";
+import ReactLogo from "@/assets/img/react-logo.svg";
+import NextjsLogo from "@/assets/img/nextjs-logo.svg";
+import TypeScriptLogo from "@/assets/img/typescript-logo.svg";
+import JavaScriptLogo from "@/assets/img/javascript-logo.svg";
+import MongoDBLogo from "@/assets/img/mongodb-logo.svg";
+import FirebaseLogo from "@/assets/img/firebase-logo.svg";
+import NodeLogo from "@/assets/img/nodejs-logo.svg";
+import TailwindLogo from "@/assets/img/tailwind-logo.svg";
+import PrismaLogo from "@/assets/img/prisma-logo.svg";
+
+const techStack = [
+  { name: "React", Component: ReactLogo },
+  { name: "NextJS", Component: NextjsLogo },
+  { name: "Node", Component: NodeLogo },
+  { name: "MongoDB", Component: MongoDBLogo },
+  { name: "JavaScript", Component: JavaScriptLogo },
+  { name: "Firebase", Component: FirebaseLogo },
+  { name: "TypeScript", Component: TypeScriptLogo },
+  { name: "Tailwind", Component: TailwindLogo },
+  { name: "Prisma", Component: PrismaLogo },
+];
 
 const Projects = () => {
   return (
@@ -8,13 +29,19 @@ const Projects = () => {
           <h2>Tools</h2>
         </div>
         <div className={styles.tools_imageContainer}>
-          <div className={styles.imageContainer}>
-            
-          </div>
+          {techStack.map((tech, index) => {
+            const Logo = tech.Component; // Renombrado para mayor claridad
+            return (
+              <div className={styles.imageContainer_wrapper} key={index}>
+                <img src={Logo} alt={tech.name} className={styles.wrapper_image} />
+                <span className={styles.wrapper_image__text}>{tech.name}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className={styles.projectsContainer__imagesProject}>
-      <div className={styles.imageProject_wrapper}>
+        <div className={styles.imageProject_wrapper}>
           <div className={styles.wrapper_imageContainer}></div>
           <div className={styles.wrapper_textContainer}></div>
         </div>
@@ -24,7 +51,7 @@ const Projects = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
