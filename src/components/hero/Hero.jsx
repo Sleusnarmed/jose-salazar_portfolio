a2727725ad68eb2303cbd569b18db8ff7f727ee5
Jose Salazar <jmsalazar.macg@hotmail.com>
2025-12-03 15:00:35 -0600

    model 3d with interaction

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,30 +1,31 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
-import { Suspense } from 'react';
-import modeloUrl from '@/assets/3d-models/oficina_setup.glb';
-
-function Modelo3D() {
-  const { scene } = useGLTF(modeloUrl);
-  // Escala y posiciona según necesites
-  return <primitive object={scene} position={[0, -1, 0]} scale={0.5} />;
-}
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import styles from "./hero.module.css";
+import { Suspense } from "react";
+import { Model } from "../ModeloOficina.tsx";
 
 const HeroSection = () => {
   return (
-    <div style={{ width: '100vw', height: '50vh' }}>
-      <Canvas camera={{ position: [5, 5, 5], fov: 50 }}>
+    <div className={styles.modelContainer}>
+      <Canvas
+        className={styles.canva}
+        shadows
+        gl={{ antialias: true }}
+        camera={{ position: [2, 2, 2], fov: 35 }}
+      >
         <Suspense fallback={null}>
-          <Modelo3D />
-          <Environment preset="city" />
-
-        
+          <Model position={[0, -0.4, 0]} scale={0.5} />
+          <Environment preset="sunset" />
         </Suspense>
-        <OrbitControls enableZoom={true} enablePan={true} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          minDistance={1.75}
+          maxDistance={3}
+        />
       </Canvas>
     </div>
   );
 };
-// Precarga el modelo para mejor rendimiento
-useGLTF.preload(modeloUrl);
 
 export default HeroSection;
